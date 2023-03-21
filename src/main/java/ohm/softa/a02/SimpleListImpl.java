b537d51a74fd56c08ffaef7425945830dadd411b
nico <->
2023-03-21 10:37:04 +0100

    basics

--- src/main/java/ohm/softa/a02/SimpleListImpl.java
+++ src/main/java/ohm/softa/a02/SimpleListImpl.java
@@ -1,11 +1,51 @@
 package ohm.softa.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable<Object> {
+    @Override
+    public void add(Object o) {
 
-	// TODO: Implement the required methods.
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return new ListIterator();
+    }
+
+    private static class ListElement {
+        private Object o;
+
+        public ListElement(Object o) {
+            this.o = o;
+        }
+    }
+
+    public class ListIterator implements Iterator<Object> {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
 
 }
